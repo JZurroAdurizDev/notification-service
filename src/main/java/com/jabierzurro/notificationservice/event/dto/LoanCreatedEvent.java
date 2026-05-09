d2f61b740e49111628d178884c2db53965232640
Jabier Zurro Aduriz <jabier_zurro@hotmail.com>
2026-05-09 20:20:25 +0100

    docs: add Javadoc documentation through microservice layers

--- src/main/java/com/jabierzurro/notificationservice/event/dto/LoanCreatedEvent.java
+++ src/main/java/com/jabierzurro/notificationservice/event/dto/LoanCreatedEvent.java
@@ -5,6 +5,18 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 /**
+ * DTO representing a loan creation event transferred through Kafka.
+ *
+ * <p>This event is published by library-api and consumed by notification-service
+ * to simulate notification delivery asynchronously.
+ *
+ * @param loanId identifier of the created loan
+ * @param userId identifier of the user who created the loan
+ * @param userEmail email address of the user
+ * @param bookTitles titles of the books included in the loan
+ * @param startDate loan start date
+ * @param dueDate loan due date
+ * @param timestamp event creation timestamp
  *
  * @author Jabier Zurro Aduriz
  */
